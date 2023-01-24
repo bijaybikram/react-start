eeaea53a78a8c585fc0eafd074b83fea0a95ad2c
bijaybikram <60894605+bijaybikram@users.noreply.github.com>
2023-01-24 10:27:42 +0545

    Added file for dynamic title
    A title is made auto changing

--- App.js
+++ App.js
@@ -1,15 +1,77 @@
-import About from "./components/About";
+import { type } from "@testing-library/user-event/dist/type";
+import { useState } from "react";
+import Alert from "./components/Alert";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
-  
+import TextForm from "./components/TextForm";
+
 function App() {
+
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1200);
+  }
+
+  const toggleMode = () => {
+    if(mode==='light'){
+      setMode ('dark');
+      document.body.style.backgroundColor='grey';
+      showAlert("Dark mode is enabled","success");
+      // changing page title using js
+      document.title = 'TextUtils - Dark Mode';
+
+      // To make a blinking dynamic title 
+      // setInterval(() => {
+      //   document.title = 'TextUtils is an amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils right now!';
+      // }, 1500);
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode is enabled","primary");
+      
+    }
+    
+  }
+
+  const joggleMode = () => {
+    if(mode==='light'){
+      setMode ('dark');
+      document.body.style.backgroundColor='blueviolet';
+      showAlert("Blue mode is enabled","success");
+      document.title = 'TextUtils - Blue Mode';
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode is enabled","primary");
+    }
+    
+  }
   return (
     <>
     
-<Navbar title="TextUtils" aboutText="About Us"/>
+{/* <Navbar title="TextUtils" aboutText="About Us"/> */}
+
+<Navbar title="TextUtils" aboutText="About" mode= {mode} toggleMode={toggleMode} joggleMode={joggleMode}/>
+<Alert alert={alert}/>
+
 <div className="container my-3" >
-  {/* <TextForm heading = "Enter the text to analyze"/> */}
-  <About/>
+
+<TextForm showAlert={showAlert} heading = "Enter the text to analyze" mode= {mode}/>
+
+{/* <About/> */}
 </div>
 
     </>
--- TextForm.js
+++ TextForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 
 
 export default function TextForm(props) {
@@ -7,17 +7,20 @@ export default function TextForm(props) {
         // console.log('uppercase was clcked' + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase","success")
     }
     const handleLowClick = () =>{
         // console.log('uppercase was clcked' + text);
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to lowercase","success")
     }
 
     const handleClearClick = () =>{
         // console.log('uppercase was clcked' + text);
         let newText = "";
         setText(newText);
+        props.showAlert("Text area cleared","success")
     }
 
     // function to speak
@@ -25,6 +28,7 @@ export default function TextForm(props) {
         let msg = new SpeechSynthesisUtterance();
         msg.text = text;
         window.speechSynthesis.speak(msg);
+        props.showAlert("Text converting to speech","success")
     }
 
     // funcion to count vowels
@@ -33,6 +37,7 @@ export default function TextForm(props) {
             if(text.charAt(count).match(/[aeiouAEIOU]/)){
                 countVo++;
                 setCount(countVo);
+                props.showAlert("Vowel counted","success")
             }
         }
         // console.log("no of vowels are:"+ countVo);
@@ -44,6 +49,7 @@ export default function TextForm(props) {
             if(text.charAt(count1).match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)){
                 countCons++;
                 setCount1(countCons);
+                props.showAlert("Consonant counted","success")
             }
         }
         // console.log("no of vowels are:"+ countVo);
@@ -53,6 +59,7 @@ export default function TextForm(props) {
         // console.log('uppercase was clcked' + text);
         var newText = document.getElementById('myBox');
         newText.style.color="green";
+        props.showAlert("Text color is changed","success")
         
     }
 
@@ -61,7 +68,21 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to clipboard","success")
     }
+
+    //array of words
+    
+    const wordCount = ()=>{
+        if (text.length>0){
+        return text.trim().split(/[ ]+/).length;
+        }
+        else{
+        return 0;
+        }
+        }
+        
+    
 
    
     const handleOnChange = (event) =>{
@@ -71,33 +92,36 @@ export default function TextForm(props) {
 
    
     // global declarations
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState("Enter text here");
+
     let [count, setCount] = useState(0);
     let [count1, setCount1] = useState(0);
     let countVo = 0,
         countCons = 0;
     // setText("Ramey ko nati");
+
+
     
   return (
     <>
-        <div className='container'>
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading} </h1>
         <div className="mb-3">
-            <textarea className="form-control" value ={text} onChange={handleOnChange} id="myBox" cols="30" rows="8"></textarea>
+            <textarea className="form-control" value ={text} style={{backgroundColor: props.mode==='dark'?'grey':'white' , color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" cols="30" rows="8"></textarea>
         </div>
-        <button className='btn.btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary ' onClick={handleUpClick}>Convert to Uppercase</button>
 
-        <button className='btn.btn-primary mx-3' onClick={handleLowClick}>Convert to Lowercase</button>
+        <button className='btn btn-primary mx-3' onClick={handleLowClick}>Convert to Lowercase</button>
 
-        <button className='btn.btn-primary mx-3' onClick={handleClearClick}>Clear</button>
+        <button className='btn btn-primary mx-3' onClick={handleClearClick}>Clear</button>
 
-        <button className='btn.btn-primary mx-3' onClick={handleVoClick}>Count Vowels</button>
+        <button className='btn btn-primary mx-3' onClick={handleVoClick}>Count Vowels</button>
 
-        <button className='btn.btn-primary mx-3' onClick={handleConsClick}>Count Consonants</button>
+        <button className='btn btn-primary mx-3' onClick={handleConsClick}>Count Consonants</button>
 
-        <button className='btn.btn-primary mx-3' onClick={handleColorClick}>Color Change</button>
+        <button className='btn btn-primary mx-3' onClick={handleColorClick}>Color Change</button>
 
-        <button className='btn.btn-primary mx-3' onClick={handleCopy}>Copy Text</button>
+        <button className='btn btn-primary mx-3' onClick={handleCopy}>Copy Text</button>
 
         
 
@@ -105,16 +129,16 @@ export default function TextForm(props) {
 
     </div>
 
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words & {text.length} characters</p>
+        <p>{wordCount()} words & {text.length} characters</p>
         <p>{0.008 * text.split(" ").length}
              Minutes to read </p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text: "Enter something in the text box above to preview it here"}</p>
     </div>
 
-    <div className='container mx-3 my-3'>
+    <div className='container mx-3 my-3' style={{color: props.mode==='dark'?'white':'black'}}>
         <h2>Your vowel entered</h2>
         <p>{count} No. of vowels</p>
         <p>{count1} No. of consonants</p>
