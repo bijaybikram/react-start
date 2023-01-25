629e028dfd00b97f88a37f07d67523a3347fc6a2
bijaybikram <60894605+bijaybikram@users.noreply.github.com>
2023-01-25 14:59:19 +0545

    Ready for deployment

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import './App.css';
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1200);
+  }
+
+  const toggleMode = () => {
+    if(mode==='light'){
+      setMode ('dark');
+      document.body.style.backgroundColor='grey';
+      showAlert("Dark mode is enabled","success");
+      // changing page title using js
+      document.title = 'TextUtils - Dark Mode';
+
+      // To make a blinking dynamic title 
+      // setInterval(() => {
+      //   document.title = 'TextUtils is an amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils right now!';
+      // }, 1500);
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode is enabled","primary");
+      
+    }
+    
+  }
+
+  const joggleMode = () => {
+    if(mode==='light'){
+      setMode ('dark');
+      document.body.style.backgroundColor='blueviolet';
+      showAlert("Blue mode is enabled","success");
+      document.title = 'TextUtils - Blue Mode';
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode is enabled","primary");
+    }
+    
+  }
+  return (
+    <>
+    
+{/* <Navbar title="TextUtils" aboutText="About Us"/> */}
+<Router>
+<Navbar title="TextUtils" aboutText="About" mode= {mode} toggleMode={toggleMode} joggleMode={joggleMode}/>
+<Alert alert={alert}/>
+
+<div className="container my-3" >
+
+  <Routes>
+      <Route path="/about" element={<About/>} />
+      <Route path="/TextForm" element={<TextForm showAlert={showAlert} heading = "Enter the text to analyze" mode= {mode}/>} />
+      
+      
+  </Routes>
+
+</div>
+</Router>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,152 @@
+import React, {useState} from 'react';
+
+
+export default function TextForm(props) {
+
+    const handleUpClick = () =>{
+        // console.log('uppercase was clcked' + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Uppercase","success")
+    }
+    const handleLowClick = () =>{
+        // console.log('uppercase was clcked' + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lowercase","success")
+    }
+
+    const handleClearClick = () =>{
+        // console.log('uppercase was clcked' + text);
+        let newText = "";
+        setText(newText);
+        props.showAlert("Text area cleared","success")
+    }
+
+    // function to speak
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+        props.showAlert("Text converting to speech","success")
+    }
+
+    // funcion to count vowels
+    const handleVoClick = () =>{
+        for(count=0; count<=text.length; count++){
+            if(text.charAt(count).match(/[aeiouAEIOU]/)){
+                countVo++;
+                setCount(countVo);
+                props.showAlert("Vowel counted","success")
+            }
+        }
+        // console.log("no of vowels are:"+ countVo);
+    };
+
+    // funcion to count consonants
+    const handleConsClick = () =>{
+        for(count1=0; count1<=text.length; count1++){
+            if(text.charAt(count1).match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)){
+                countCons++;
+                setCount1(countCons);
+                props.showAlert("Consonant counted","success")
+            }
+        }
+        // console.log("no of vowels are:"+ countVo);
+    };
+
+    const handleColorClick = () =>{
+        // console.log('uppercase was clcked' + text);
+        var newText = document.getElementById('myBox');
+        newText.style.color="green";
+        props.showAlert("Text color is changed","success")
+        
+    }
+
+    const handleCopy = () => {
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to clipboard","success")
+    }
+
+    //array of words
+    
+    const wordCount = ()=>{
+        if (text.length>0){
+        return text.trim().split(/[ ]+/).length;
+        }
+        else{
+        return 0;
+        }
+        }
+        
+    
+
+   
+    const handleOnChange = (event) =>{
+        // console.log('on change');
+        setText(event.target.value);
+    }
+
+   
+    // global declarations
+    const [text, setText] = useState("Enter text here");
+
+    let [count, setCount] = useState(0);
+    let [count1, setCount1] = useState(0);
+    let countVo = 0,
+        countCons = 0;
+    // setText("Ramey ko nati");
+
+
+    
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>{props.heading} </h1>
+        <div className="mb-3">
+            <textarea className="form-control" value ={text} style={{backgroundColor: props.mode==='dark'?'grey':'white' , color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" cols="30" rows="8"></textarea>
+        </div>
+        <button className='btn btn-primary ' onClick={handleUpClick}>Convert to Uppercase</button>
+
+        <button className='btn btn-primary mx-3' onClick={handleLowClick}>Convert to Lowercase</button>
+
+        <button className='btn btn-primary mx-3' onClick={handleClearClick}>Clear</button>
+
+        <button className='btn btn-primary mx-3' onClick={handleVoClick}>Count Vowels</button>
+
+        <button className='btn btn-primary mx-3' onClick={handleConsClick}>Count Consonants</button>
+
+        <button className='btn btn-primary mx-3' onClick={handleColorClick}>Color Change</button>
+
+        <button className='btn btn-primary mx-3' onClick={handleCopy}>Copy Text</button>
+
+        
+
+        <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+
+    </div>
+
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h2>Your text summary</h2>
+        <p>{wordCount()} words & {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length}
+             Minutes to read </p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text: "Enter something in the text box above to preview it here"}</p>
+    </div>
+
+    <div className='container mx-3 my-3' style={{color: props.mode==='dark'?'white':'black'}}>
+        <h2>Your vowel entered</h2>
+        <p>{count} No. of vowels</p>
+        <p>{count1} No. of consonants</p>
+    </div>
+
+    </>
+    
+  )
+ }
+
+
