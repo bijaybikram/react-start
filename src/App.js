5bc976cc02745a05be1264c8cdf210df047a6756
bijaybikram <60894605+bijaybikram@users.noreply.github.com>
2023-01-25 15:25:58 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 // import {
